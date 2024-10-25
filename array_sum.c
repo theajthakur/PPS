1d46d713a441fb59d4b761de0c20fa7a0c3e3662
Vijay Singh <thakurvijayofficial@gmail.com>
2024-10-25 12:41:27 +0530

    Make Array in Loop and then Add

---
+++ array_sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+    int length=10;
+    int a[length];
+    int sum=0;
+    for (int i = 0; i < length; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < length; i++)
+    {
+        sum+=a[i];
+    }
+    printf("%d", sum);
+}
