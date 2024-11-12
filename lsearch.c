2201a5cbdcaf873110649d2ecdee5ef2b756f02e
theajthakur <thakurvijayofficial@gmail.com>
2024-11-12 16:54:36 +0530

    Lsearch

---
+++ lsearch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void showArray(int arr[], int num){
+    for(int i=0; i<num; i++){
+        printf("%d \t", arr[i]);
+    }
+}
+
+void main(){
+    int n=10;
+    int arr[10]={9,8,7,6,5,4,3,2,1,-1};
+    int tc=0;
+    for (int i = 0; i < (n); i++){
+        for (int j = 0; j < (n-i); j++){
+            tc++;
+            if(arr[j]>arr[j+1]){
+                int tmp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=tmp;
+            }
+        }
+    }
+    showArray(arr, n);
+    printf("\n%d", tc);
+}
