1f91572d16cd17294384a2abd33db07d71d6a278
theajthakur <thakurvijayofficial@gmail.com>
2024-11-26 16:44:12 +0530

    Proto of Binary Search

---
+++ binary.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int arr[]={2,8,16,14,19,23};
+    int element=0;
+    printf("Enter Elemen: ");
+    scanf("%d",&element);
+    printf("Entered: %d", element);
+}
